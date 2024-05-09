1f584eeda31dfe0dae6d7ecd266bbc17cb8fa2e7
Laycon John <laycon.gomes@tld.com.br>
2024-05-09 17:18:50 -0300

    feat: tests Submitting an image with valid inputs using enter key

--- cypress-project/cypress/e2e/spec.cy.js
+++ cypress-project/cypress/e2e/spec.cy.js
@@ -6,7 +6,8 @@ class RegisterForm {
     titleFeedback: () => cy.get("#titleFeedback"),
     imageUrlInput: () => cy.get("#imageUrl"),
     urlFeedback: () => cy.get("#urlFeedback"),
-    submitBtn: () => cy.get('#btnSubmit')
+    submitBtn: () => cy.get('#btnSubmit'),
+    imagesConteiner: () => cy.get("#card-list")
   }
 
   typeTitle(title) {
@@ -24,13 +25,23 @@ class RegisterForm {
   clickSubmit() {
     this.elements.submitBtn().click()
   }
+
+  clickEnter() {
+    cy.focused().type('{enter}')
+  }
 }
 
 const registerForm = new RegisterForm()
 
 const colors = {
   error: "rgb(220, 53, 69)",
-  sucess: ""
+  sucess: "rgb(25, 135, 84)"
+}
+
+const baseUrl = "https://erickwendel.github.io" 
+
+const localStorage = {
+  imageRegistration: "tdd-ew-db"
 }
 
 describe('Image Registration', () => {
@@ -79,4 +90,69 @@ describe('Image Registration', () => {
 
     
   })
+
+  describe('Submitting an image with valid inputs using enter key', () => {
+    after(() => {
+      cy.clearAllLocalStorage()
+    })
+
+    const input = {
+      title: "Alien BR",
+      url: "https://cdn.mos.cms.futurecdn.net/eM9EvWyDxXcnQTTyH8c8p5-1200-80.jpg",
+    }
+
+    it('Given I am on the image registration page', () => {
+      cy.visit('/')
+    })
+
+    it("When I enter 'Alien BR' in the title field", () => {
+      registerForm.typeTitle(`${input.title}{enter}`)
+    })
+
+    it("When I enter 'https://cdn.mos.cms.futurecdn.net/eM9EvWyDxXcnQTTyH8c8p5-1200-80.jpg' in the URL field", () => {
+      registerForm.typeUrl(input.url)
+    })
+
+    it("Then I should see a check icon in the title field", () => {
+      registerForm.elements.titleInput().should(([value]) => {
+        const styles = window.getComputedStyle(value)
+        const border = styles.getPropertyValue('border-right-color')
+        assert.strictEqual(border, colors.sucess)
+      })
+    })
+
+    it("Then I should see a check icon in the imageUrl field", () => {
+      registerForm.elements.imageUrlInput().should(([value]) => {
+        const styles = window.getComputedStyle(value)
+        const border = styles.getPropertyValue('border-right-color')
+        assert.strictEqual(border, colors.sucess)
+      })
+    })
+
+    it("Then I can hit enter to submit the form", () => {
+      registerForm.clickEnter()
+      cy.wait(1000)
+    })
+
+    it("And the list of registered images should be updated with the new item", () => {
+      registerForm.elements.imagesConteiner().children().should("have.length", 4)
+    })
+
+    it("And the new item should be stored in the localStorage", () => {
+      cy.getAllLocalStorage().then((result) => {
+        const localStorageImageRegistrationValue = result[baseUrl][localStorage.imageRegistration]
+        
+        const expectedValue = `[{"title":"Alien BR","imageUrl":"https://cdn.mos.cms.futurecdn.net/eM9EvWyDxXcnQTTyH8c8p5-1200-80.jpg"}]`
+        assert.strictEqual(localStorageImageRegistrationValue, expectedValue)
+
+
+      })
+    })
+
+    it("Then The inputs should be cleared", () => {
+      registerForm.elements.titleInput().should("be.empty")
+      registerForm.elements.imageUrlInput().should("be.empty")
+    })
+  })
 })
+
